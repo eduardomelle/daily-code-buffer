ef90e24f9ef7e39034e1c12de6ed3549072715a4
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-04-24 06:52:58 -0300

    Connecting Multiple services via HttpExchange

--- department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
@@ -33,7 +33,7 @@ public class DepartmentController {
     }
 
     @GetMapping("/{id}")
-    public Department findById(@PathVariable Long id) {
+    public Department findById(@PathVariable("id") Long id) {
         LOGGER.info("Department find: id={}", id);
         return repository.findById(id);
     }
--- employee-service/src/main/java/com/dailycodebuffer/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/dailycodebuffer/employeeservice/controller/EmployeeController.java
@@ -1,17 +1,47 @@
 package com.dailycodebuffer.employeeservice.controller;
 
+import com.dailycodebuffer.employeeservice.model.Employee;
 import com.dailycodebuffer.employeeservice.repository.EmployeeRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+
     private final EmployeeRepository repository;
 
     public EmployeeController(EmployeeRepository repository) {
         this.repository = repository;
+    }
+
+    @PostMapping
+    public Employee add(@RequestBody Employee employee) {
+        LOGGER.info("Employee add: {}", employee);
+        return repository.add(employee);
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        LOGGER.info("Employee find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Long id) {
+        LOGGER.info("Employee find: id={}", id);
+        return repository.findById(id);
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public List<Employee> findByDepartmentId(@PathVariable("departmentId") Long departmentId) {
+        LOGGER.info("Employee find: departmentId={}", departmentId);
+        return repository.findByDepartment(departmentId);
     }
 
 }
