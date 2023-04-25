da8f3acd2623d1e6fb11f43e3954f6e8db9f9f5c
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-04-24 21:03:30 -0300

    Connecting Multiple services via HttpExchange

--- department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.departmentservice.controller;
 
+import com.dailycodebuffer.departmentservice.client.EmployeeClient;
 import com.dailycodebuffer.departmentservice.model.Department;
 import com.dailycodebuffer.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
@@ -16,8 +17,11 @@ public class DepartmentController {
 
     private final DepartmentRepository repository;
 
-    public DepartmentController(DepartmentRepository repository) {
+    private final EmployeeClient employeeClient;
+
+    public DepartmentController(DepartmentRepository repository, EmployeeClient employeeClient) {
         this.repository = repository;
+        this.employeeClient = employeeClient;
     }
 
     @PostMapping
@@ -36,6 +40,14 @@ public class DepartmentController {
     public Department findById(@PathVariable("id") Long id) {
         LOGGER.info("Department find: id={}", id);
         return repository.findById(id);
+    }
+
+    @GetMapping("/with-employees")
+    public List<Department> findAllWithEmployees() {
+        LOGGER.info("Department find");
+        List<Department> departments = repository.findAll();
+        departments.forEach(department -> department.setEmployees(employeeClient.findByDepartmentId(department.getId())));
+        return departments;
     }
 
 }
